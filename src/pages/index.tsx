0f31e21ac50a8ca360207e48c856e49ce2792cf8
Nick <34172910+NickGeronimo@users.noreply.github.com>
2024-05-17 19:42:38 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,6 @@ Nicholas Geronimo</a></h1>
     <section id="hero" className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12">
       <article className="sm:w-1/2">
         <h2 className="max-w-md text-4xl font-bold text-center sm:text-5xl sm:text-left text-slate-900 dark:text-white">
-          We Boldy Go <span className="text-indigo-700 dark:text-indigo-300"> Where No Rocket</span> Has Gone Before...
         </h2>
       </article>
     </section>
